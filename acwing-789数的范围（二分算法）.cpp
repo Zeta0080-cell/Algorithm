200575beb94f926a484a38637be1b40f0b8cdad6
Zeta0080-cell <2489889104@qq.com>
2025-07-06 21:43:14 +0800

    Add files via upload

---
+++ acwing-789数的范围（二分算法）.cpp
@@ -0,0 +1,58 @@
+//acwing-789数的范围（二分算法）
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e6;
+int a[N];
+
+int main()
+{
+	int n,q;//n个数，q次查询
+	int x;
+	cin>>n>>q;
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	while(q--)
+	{
+		cin>>x;
+		int l=0;
+		int r=n-1;
+		while(l<r)
+		{
+			int mid=(l+r)>>1;
+			if(a[mid]>=x)
+			{
+				r=mid;
+			}
+			else
+			{
+				l=mid+1;
+			}
+		}
+		if(a[l]!=x)
+		{
+			printf("-1 -1\n");
+		}
+		else
+		{
+			printf("%d ",l);
+			int l=0;
+			int r=n-1;
+			while(l<r)
+			{
+				int mid=l+r+1>>1;
+				if(a[mid]<=x)
+				{
+					l=mid;
+				}
+				else
+				{
+					r=mid-1;
+				}
+			}
+			printf("%d\n",l);
+		}
+	}
+	return 0;
+} 
