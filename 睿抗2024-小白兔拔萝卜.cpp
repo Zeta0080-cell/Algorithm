182adb0b38de7ccd793898663c7a75a0749f2158
Zeta0080-cell <2489889104@qq.com>
2025-07-11 14:33:02 +0800

    Add files via upload

---
+++ 睿抗2024-小白兔拔萝卜.cpp
@@ -0,0 +1,18 @@
+//î£¿¹2024-Ğ¡°×ÍÃ°ÎÂÜ²·
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+    if(n>=m)
+	{
+		printf("Ba!\n%d",n-m);	
+	}	 
+	else
+	{
+		printf("Suan4 le ba.\n%d",m-n);
+	}
+	return 0;
+} 
