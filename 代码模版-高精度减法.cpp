32ce29f7ed57e51767c0dc4e94418a0a0981f096
Zeta0080-cell <2489889104@qq.com>
2025-07-14 22:11:16 +0800

    Add files via upload

--- 代码模版-高精度减法.cpp
+++ 代码模版-高精度减法.cpp
@@ -2,79 +2,79 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool cmp(vector<int> a,vector<int> b)
+bool cmp(vector<int> a, vector<int> b) 
 {
-	if(a.size()>b.size())
+    if (a.size() != b.size()) 
 	{
-		return true;
-	}
-	else if(a.size()==b.size())
+        return a.size() > b.size();
+    }
+    for (int i = a.size() - 1; i >= 0; i--) 
 	{
-		for(int i=a.size()-1;i>=0;i--)
+        if (a[i] != b[i]) 
 		{
-			if(a[i]<b[i])
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-	else if(a.size()<b.size())
-	{
-		return false;
-	}
+            return a[i] > b[i];
+        }
+    }
+    return true; // 相等时返回 true
 }
 
-vector<int> sub(vector<int> a,vector<int> b)
+vector<int> sub(vector<int> a, vector<int> b) 
 {
-	int t=0;
-	vector<int> c;
-	for(int i=0;i<a.size();i++)
+    vector<int> c;
+    int t = 0;
+    for (int i = 0; i < a.size(); i++) 
 	{
-		t=a[i]-b[i]+10;
-		c.push_back(t%10);
-		t/=10;
-	}
-	while(c.size()>0)
-	{
-		if(c.back()==0)
+        t = a[i] - t;
+        if (i < b.size()) 
 		{
-			c.pop_back();
-		}
-	}
-	return c;
+            t -= b[i];
+        }
+        c.push_back((t + 10) % 10);
+        if (t < 0) 
+		{
+            t = 1;
+        } 
+		else 
+		{
+            t = 0;
+        }
+    }
+    while (c.size() > 1 && c.back() == 0) 
+	{
+        c.pop_back();
+    }
+    return c;
 }
 
-int main()
+int main() 
 {
-	string A,B;
-	vector<int> a,b;
-	cin>>A;
-	cin>>B;
-	for(int i=A.size()-1;i>=0;i--)
+    string A, B;
+    vector<int> a, b;
+    cin >> A >> B;
+    for (int i = A.size() - 1; i >= 0; i--) 
 	{
-		a.push_back(A[i]-'0');	
-	}
-	for(int i=B.size()-1;i>=0;i--)
+        a.push_back(A[i] - '0');
+    }
+    for (int i = B.size() - 1; i >= 0; i--) 
 	{
-		b.push_back(B[i]-'0');
-	}
-	if(cmp(a,b))
+        b.push_back(B[i] - '0');
+    }
+    if (cmp(a, b)) 
 	{
-		vector<int> c=sub(a,b);
-		for(int i=c.size()-1;i>=0;i--)
+        vector<int> c = sub(a, b);
+        for (int i = c.size() - 1; i >= 0; i--) 
 		{
-			printf("%d",c[i]);
-		}
-	}
-	else
+            printf("%d", c[i]);
+        }
+    } 
+	else 
 	{
-		vector<int> c=sub(a,b);
-		printf("-");
-		for(int i=c.size()-1;i>=0;i--)
+        vector<int> c = sub(b, a);
+        printf("-");
+        for (int i = c.size() - 1; i >= 0; i--) 
 		{
-			printf("%d",c[i]);
-		}
-	}
-	return 0;
+            printf("%d", c[i]);
+        }
+    }
+    return 0;
 }
