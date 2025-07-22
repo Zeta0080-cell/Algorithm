3976c1cdf6f8b188e00b526476b3a79be07689a8
Zeta0080-cell <2489889104@qq.com>
2025-07-22 19:52:25 +0800

    Add files via upload

---
+++ acwing2816-判断子序列.cpp
@@ -0,0 +1,37 @@
+//acwing2816-еп╤овспРап
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e3;
+int a[N],b[N];
+
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(int i=0;i<m;i++)
+	{
+		scanf("%d",&b[i]);
+	}
+	int i=0,j=0;
+	while(i<n&&j<m)
+	{
+		if(a[i]==b[j])
+		{
+			i++;
+		}
+		j++;
+	}
+	if(i==n)
+	{
+		printf("Yes");
+	}
+	else
+	{
+		printf("No");	
+	}
+	return 0;
+} 
