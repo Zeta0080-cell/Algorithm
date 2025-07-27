21500fac484b47bf54ee4e0849a4b003d2ca7724
Zeta0080-cell <2489889104@qq.com>
2025-07-27 16:13:47 +0800

    Add files via upload

---
+++ acwing829-模拟队列(伪代码).cpp
@@ -0,0 +1,31 @@
+//acwing829-模拟队列(伪代码) 
+#include<bits/std++.h>
+using namespace std;
+const int N=100010;
+int stk[N],tt;
+
+//插入
+stk[++tt]=x;
+
+//弹出
+tt--;
+
+//判断栈是否为空
+if(tt>0) not empty
+else empty
+
+//栈顶
+stk[tt];
+
+//在队尾插入元素，在队头弹出元素
+int q[N],hh,tt=-1;
+
+//插入
+q[++tt]=x;
+
+//弹出
+hh++;
+
+//判断队列是否为空
+if(hh<=tt) not empty
+else empty 
