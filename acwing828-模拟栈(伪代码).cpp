be32308872d42d676af89b882280e08c7b34a181
Zeta0080-cell <2489889104@qq.com>
2025-07-27 13:49:44 +0800

    Add files via upload

---
+++ acwing828-模拟栈(伪代码).cpp
@@ -0,0 +1,18 @@
+//acwing828-模拟栈(伪代码) 
+#include<bits/stdc++.h>
+using namespace std;
+const int N=100010;
+int stk[N],tt;
+
+//插入操作 
+stk[++tt]=x;
+
+//弹出
+tt--;
+
+//判断栈是否为空
+if(tt>0) not empty
+else empty
+
+//栈顶
+stk[tt] 
