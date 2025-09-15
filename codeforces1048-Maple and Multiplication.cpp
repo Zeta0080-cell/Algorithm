374576de891e7b90f266ec9824bedbd784a728cf
Zeta0080-cell <2489889104@qq.com>
2025-09-15 10:17:58 +0800

    Add files via upload

---
+++ codeforces1048-Maple and Multiplication.cpp
@@ -0,0 +1,27 @@
+//codeforces1048-Maple and Multiplication
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--) 
+	{
+        int a, b;
+        cin >> a >> b;
+        if (a == b) 
+		{
+            cout << 0 << endl;
+        } 
+		else if (b % a == 0 || a % b == 0) 
+		{
+            cout << 1 << endl;
+        } 
+		else 
+		{
+            cout << 2 << endl;
+        }
+    }
+    return 0;
+}
