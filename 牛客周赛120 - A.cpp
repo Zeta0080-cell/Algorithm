45d5c391beabb24a66746ef62049fc105ce09b8f
Zeta0080-cell <2489889104@qq.com>
2025-12-01 17:24:17 +0800

    Add files via upload

---
+++ 牛客周赛120 - A.cpp
@@ -0,0 +1,19 @@
+//Å£¿ÍÖÜÈü120 - A
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for(int i = 1; i <= n; i ++)
+	{
+		printf("a");
+	}
+	printf("b");
+	for(int i = 1; i <= n; i ++)
+	{
+		printf("a");
+	}
+	return 0;
+} 
