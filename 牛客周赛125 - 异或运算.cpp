b820dd672c9d1b855c824761882322339a4bd4ec
Zeta0080-cell <2489889104@qq.com>
2026-01-15 16:10:29 +0800

    Add files via upload

--- 牛客周赛125 - 异或运算.cpp
+++ 牛客周赛125 - 异或运算.cpp
@@ -1,37 +1,48 @@
 // 牛客周赛125 - 异或运算
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int T;
-    cin >> T;
-    while (T--) {
-        int n, m;
-        string A, B;
-        cin >> n >> m >> A >> B;
-        
-        int xorA = 0, xorB = 0;
-        for (char c : A) xorA ^= (c - '0');
-        for (char c : B) xorB ^= (c - '0');
-        
-        if (xorA != xorB) {
-            cout << "NO\n";
-            continue;
+typedef long long ll;
+void solve()
+{
+    ll n,m;
+    cin>>n>>m;
+    string a,b;
+    queue<int> A,B;
+    cin>>a;
+    cin>>b;
+    for(ll i=0;i<n;i++)    A.push(a[i]-'0');
+    for(ll i=0;i<m;i++)    B.push(b[i]-'0');
+    while(A.size()&&B.size())
+    {
+        if(A.front()==B.front())
+        {
+            A.pop();
+            B.pop();
         }
-        
-        int i = n - 1, j = m - 1;
-        while (j > 0) {
-            if (i < j) break;               // 字符不够
-            if (A[i] == B[j]) i--, j--;
-            else if (i > 0 && ((A[i-1] - '0') ^ (A[i] - '0')) == (B[j] - '0'))
-                i -= 2, j--;
-            else break;
+        else
+        {
+            int x=A.front();
+            A.pop();
+            A.front()=(A.front() ^ x);
         }
-        
-        cout << (j == 0 && i >= 0 ? "YES\n" : "NO\n");
     }
-    return 0;
+    ll x=0;
+    while(A.size())
+    {
+        x^=A.front();
+        A.pop();
+    }
+    if(!x&&B.size()==0)
+        cout<<"YES";
+    else
+        cout<<"NO";
+    cout<<endl;
+}
+int main()
+{
+    ll t;
+    t=1;
+    cin>>t;
+    while(t--)
+        solve();
 } 
